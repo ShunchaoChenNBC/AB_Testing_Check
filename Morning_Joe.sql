8057793376c9eebcde8d882c67c6525789920932
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2023-03-31 15:54:27 -0400

    Create Morning_Joe.sql

---
+++ Morning_Joe.sql
@@ -0,0 +1,27 @@
+
+with cte as (SELECT 
+d.*,
+case
+when regexp_extract(post_evar78, r"premium_experiment:variant1-v4") = "premium_experiment:variant1-v4"  then "premium_experiment:variant1-v4"
+when regexp_extract(post_evar78, r"premium_experiment:control-v4") = "premium_experiment:control-v4" then "premium_experiment:control-v4" else null end as Experiments
+FROM `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.AB_Testing_draft`d
+where lower(Binge_Details) like "%morning%joe%premium%" and lower(post_evar78) like "%premium%experiment%v4%")
+
+select device_name, Experiments, count(distinct adobe_tracking_id) as accts
+from cte
+group by 1,2
+order by 1,3 desc;
+
+# Account Mutiple Groups by Experiment Check
+with cte as (SELECT 
+d.*,
+case
+when regexp_extract(post_evar78, r"premium_experiment:variant1-v4") = "premium_experiment:variant1-v4"  then "premium_experiment:variant1-v4"
+when regexp_extract(post_evar78, r"premium_experiment:control-v4") = "premium_experiment:control-v4" then "premium_experiment:control-v4" else null end as Experiments
+FROM `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.AB_Testing_draft`d
+where lower(Binge_Details) like "%morning%joe%premium%" and lower(post_evar78) like "%premium%experiment%v4%")
+
+select adobe_tracking_id, count(distinct Experiments) as accts
+from cte
+group by 1
+order by 2 desc;
