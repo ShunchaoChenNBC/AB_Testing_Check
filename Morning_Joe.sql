24bbe29c202c2f494b355516718159d443f0c8db
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2023-04-06 10:07:20 -0400

    Update Morning_Joe.sql
    Brkdwn by Mth, Morning Joe + Morning Joe Premium

--- Morning_Joe.sql
+++ Morning_Joe.sql
@@ -1,16 +1,17 @@
 
+
 with cte as (SELECT 
 d.*,
 case
 when regexp_extract(post_evar78, r"premium_experiment:variant1-v4") = "premium_experiment:variant1-v4"  then "premium_experiment:variant1-v4"
 when regexp_extract(post_evar78, r"premium_experiment:control-v4") = "premium_experiment:control-v4" then "premium_experiment:control-v4" else null end as Experiments
-FROM `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.AB_Testing_draft`d
-where lower(Binge_Details) like "%morning%joe%premium%" and lower(post_evar78) like "%premium%experiment%v4%")
+FROM `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.AB_Testing_draft_03` d
+where lower(Binge_Details) like "%morning%joe%" and lower(post_evar78) like "%premium%experiment%v4%")
 
-select device_name, Experiments, count(distinct adobe_tracking_id) as accts
+select adobe_date,device_name, Experiments, count(distinct adobe_tracking_id) as accts
 from cte
-group by 1,2
-order by 1,3 desc;
+group by 1,2,3
+order by 1,2,4 desc;
 
 # Account Mutiple Groups by Experiment Check
 with cte as (SELECT 
