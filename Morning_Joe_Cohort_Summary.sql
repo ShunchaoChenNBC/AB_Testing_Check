353be382071557da4999a6b3eb7039ff518361bd
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2023-04-25 13:21:33 -0400

    Create Morning_Joe_Cohort_Summary.sql

---
+++ Morning_Joe_Cohort_Summary.sql
@@ -0,0 +1,33 @@
+with mj as (select adobe_tracking_id,
+variant
+from  `nbcu-ds-sandbox-a-001.ab_userlists.morning_news_userlist`),
+
+cte1 as (
+select adobe_tracking_id,
+adobe_date,
+display_name,
+num_seconds_played_no_ads
+from `nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_VIDEO` sv
+where adobe_date between "2023-03-28" and "2023-04-24" 
+and lower(display_name) not like "%trailer%" 
+and num_seconds_played_no_ads > 300 -- qualifier
+and adobe_tracking_id in (select distinct adobe_tracking_id from mj)
+),
+
+cte2 as (
+select adobe_tracking_id,
+count(distinct adobe_date) as Days,
+count(distinct display_name) as Repertoires,
+round(sum(num_seconds_played_no_ads)/3600,2) as Watched_Hours
+from cte1
+group by 1
+)
+
+
+select variant, 
+round(AVG(Days),2) as Avg_Days_Watched, 
+round(AVG(Watched_Hours),2) as Avg_Hours_Watched, 
+AVG(Repertoires) as Avg_Repertoires_Numbers
+from cte2
+left join mj on cte2.adobe_tracking_id = mj.adobe_tracking_id
+group by 1
